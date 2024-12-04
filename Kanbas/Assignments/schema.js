ce2fd4052337537aea9ef3197901cf83ab4f17c9
Mingle Li <limingle5@gmail.com>
2024-12-03 21:03:38 -0500

    done

---
+++ Kanbas/Assignments/schema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const assignmentSchema = new mongoose.Schema(
+    {
+        title: String,
+        course: String,
+        description: String,
+        points: Number,
+        dueDate: Date,
+        availableDate: Date
+    },
+    { collection: "assignments" }
+);
+export default assignmentSchema;
